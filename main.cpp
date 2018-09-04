3e4ea35fc73e6de0929144e3dfb1e4aaa179c230
Tomasz Warchol <twarchol@pl.sii.eu>
2018-09-04 07:58:57 +0200

    Add basic math function implementations

--- main.cpp
+++ main.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
 
+unsigned long add(int op1, int op2)
+{
+	return op1 + op2;
+}
+
+unsigned long sub(int op1, int op2)
+{
+	return op1 - op2;
+}
+
+unsigned long mul(int op1, int op2)
+{
+	return op1 * op2;
+}
+
+unsigned long div(int op1, int op2)
+{
+	return op1 / op2;
+}
+
 int main(int argc, char **argv)
 {
 	/* TODO:
